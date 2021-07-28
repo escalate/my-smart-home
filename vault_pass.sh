60c2431b68cd48b112db2ea926a13f343e6174e9
Felix Boerner <ich@felix-boerner.de>
2021-07-28 10:30:59 +0200

    feat: add vault pass script

---
+++ vault_pass.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ -f "/etc/ansible/.vault_pass.txt" ]; then
+    cat "/etc/ansible/.vault_pass.txt"
+elif [ -f "./.vault_pass.txt" ]; then
+    cat "./.vault_pass.txt"
+else
+    exit 1
+fi
