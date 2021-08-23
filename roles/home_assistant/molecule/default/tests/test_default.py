7845cd2f9f3cb8d6b547d3a2d31be8e841f3a82f
Felix Boerner <ich@felix-boerner.de>
2021-08-23 19:59:43 +0200

    feat: update home assistant role

--- roles/home_assistant/molecule/default/tests/test_default.py
+++ roles/home_assistant/molecule/default/tests/test_default.py
@@ -10,20 +10,59 @@ def test_data_directory(host):
     assert d.mode == 0o775
 
 
+def test_backup_directory(host):
+    """Check backup directory"""
+    b = host.file("/var/backups/home-assistant")
+    assert b.is_directory
+    assert b.user == "home-assistant"
+    assert b.group == "root"
+    assert b.mode == 0o775
+
+
+def test_home_assistant_config(host):
+    """Check Home Assistant config file"""
+    f = host.file("/var/lib/home-assistant/configuration.yaml")
+    assert f.is_file
+    assert f.user == "root"
+    assert f.group == "root"
+
+    config = "default_config: {}"
+    assert config in f.content_string
+
+
+def test_home_assistant_secrets(host):
+    """Check Home Assistant secrets file"""
+    f = host.file("/var/lib/home-assistant/secrets.yaml")
+    assert f.is_file
+    assert f.user == "root"
+    assert f.group == "root"
+
+    config = "password: Secr3t"
+    assert config in f.content_string
+
+
 def test_home_assistant_service(host):
-    """Check home-assistant service"""
+    """Check Home Assistant service"""
     s = host.service("home-assistant")
     assert s.is_running
     assert s.is_enabled
 
 
 def test_home_assistant_docker_container(host):
-    """Check home-assistant docker container"""
+    """Check Home Assistant docker container"""
     d = host.docker("home-assistant.service").inspect()
     assert d["HostConfig"]["Memory"] == 1073741824
     assert d["Config"]["Image"] == "homeassistant/home-assistant:latest"
     assert d["Config"]["Labels"]["maintainer"] == "me@example.com"
+    assert "PACKAGES=iputils" in d["Config"]["Env"]
     assert "internal" in d["NetworkSettings"]["Networks"]
     assert \
         "home-assistant" in \
         d["NetworkSettings"]["Networks"]["internal"]["Aliases"]
+
+
+def test_backup_cron_job(host):
+    """Check backup cron job"""
+    cmd = "/usr/local/bin/backup-home-assistant.sh"
+    f = host.file("/var/spool/cron/crontabs/root").content_string
+    assert cmd in f
