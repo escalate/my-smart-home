81f3c1891f932e515c7062bb2b5399897e230ec1
Felix Boerner <ich@felix-boerner.de>
2021-08-23 20:00:44 +0200

    feat: update telegraf role

--- roles/telegraf/molecule/default/tests/test_default.py
+++ roles/telegraf/molecule/default/tests/test_default.py
@@ -10,15 +10,37 @@ def test_config_directory(host):
     assert f.mode == 0o755
 
 
+def test_telegraf_config(host):
+    """Check Telegraf config file"""
+    f = host.file("/etc/telegraf/telegraf.conf")
+    assert f.is_file
+    assert f.user == "root"
+    assert f.group == "root"
+
+    config = (
+        "[[inputs.http_listener_v2]]\n"
+        "  service_address = \":8080\"\n"
+        "  data_format = \"influx\"\n"
+        "\n"
+        "[[outputs.prometheus_client]]\n"
+        "  listen = \":9273\"\n"
+        "  metric_version = 2\n"
+        "  flush_interval = \"10ms\"\n"
+        "  flush_jitter = \"10ms\"\n"
+        "  metric_batch_size = 1\n"
+    )
+    assert config in f.content_string
+
+
 def test_telegraf_service(host):
-    """Check telegraf service"""
+    """Check Telegraf service"""
     s = host.service("telegraf")
     assert s.is_running
     assert s.is_enabled
 
 
 def test_telegraf_docker_container(host):
-    """Check telegraf docker container"""
+    """Check Telegraf docker container"""
     d = host.docker("telegraf.service").inspect()
     assert d["HostConfig"]["Memory"] == 1073741824
     assert d["Config"]["Image"] == "telegraf:latest"
