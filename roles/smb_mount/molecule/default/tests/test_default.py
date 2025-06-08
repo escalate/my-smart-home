fb9481557f8ec012faa07b153f943e66e422b805
Felix Boerner <ich@felix-boerner.de>
2025-06-08 14:51:13 +0200

    feat: remove roles and collections

--- roles/smb_mount/molecule/default/tests/test_default.py
+++
@@ -1,8 +0,0 @@
-"""Role testing files using testinfra."""
-
-
-def test_mount_command(host):
-    """Check mount command"""
-    cmd = host.run("mount.cifs -V")
-
-    assert "mount.cifs version" in cmd.stdout
--- roles/swap/molecule/default/tests/test_default.py
+++
@@ -1,7 +0,0 @@
-"""Role testing files using testinfra"""
-
-
-def test_swap(host):
-    """Check swap"""
-    cmd = host.run("swapon --show")
-    assert "/var/swap" not in cmd.stdout
